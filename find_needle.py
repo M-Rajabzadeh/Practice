0ddf07950afca9069e5ab9dc040ab45d510802a4
Mohammad Rajabzadeh <m.rajabzadeh.works@gmail.com>
2023-11-26 00:09:00 +0330

    3 file practice

---
+++ find_needle.py
@@ -0,0 +1,23 @@
+'''
+Can you find the needle in the haystack?
+Write a function findNeedle() that takes an array full of junk but containing one "needle"
+After your function finds the needle it should return a message (as a string) that says:
+"found the needle at position " plus the index it found the needle, so:
+Example(Input --> Output)
+
+["hay", "junk", "hay", "hay", "moreJunk", "needle", "randomJunk"] --> "found the needle at position 5"
+Note: In COBOL, it should return "found the needle at position 6"
+'''
+
+def find_needle(haystack):
+    for index, item in enumerate(haystack):
+        if item == 'needle':
+            return "found the needle at position " + str(index)
+
+
+
+def find_needle(haystack):
+    return f'found the needle at position {haystack.index("needle")}'
+
+
+def find_needle(haystack): return 'found the needle at position %d' % haystack.index('needle')
